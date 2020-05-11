eeb45bb63f41b32e7ffc9f63291be6e2b3092c47
simonayele <simonayele@gmail.com>
2020-05-11 11:55:36 -0400

    icons included

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 class NoteCard extends React.Component {
   render() {
+    const { note } = this.props;
     return (
       <div className="note-card-container">
-        NoteCard Component
+        <div className="note-card-title">
+          {note.title}
+        </div>
+        <div className="note-card-content">
+          {note.content}
+        </div>
+        <span className="note-card-delet">
+          <i className="material-icon">close</i>
+        </span>
+        <span className="note-card-edit">
+          <i className="material-icons">mode_edit</i>
+        </span>
       </div>
     );
   }
